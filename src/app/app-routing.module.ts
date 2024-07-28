4715a0e9d029e04dfefa0f13e3c6ed4830f2a4f0
ZaeRon007 <julien.pelissier8@orange.fr>
2024-07-28 16:11:04 +0200

    Work In Progress : - app-component update - creation of route to the details page - creation of header component - creation of details component - dashboard styles updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: '**', // wildcard
     component: NotFoundComponent,
   },
+  {
+    path: 'detail/:id',
+    component: DetailsComponent,
+  }
 ];
 
 @NgModule({
