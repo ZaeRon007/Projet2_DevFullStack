4f9e84bc23b849de6e5f7e9231a9f7f306d316c8
ZaeRon007 <julien.pelissier8@orange.fr>
2024-07-29 12:23:48 +0200

    FIX : - addition of missing module BrowserAnimationsModule - file was missing in previous details component update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -8,14 +9,17 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { HeaderComponent } from "./header/header.component";
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [AppComponent, 
                 HomeComponent, 
                 NotFoundComponent,
                 DashboardComponent,
+                DetailsComponent,
                 HeaderComponent],
   imports: [BrowserModule,
+            BrowserAnimationsModule,
             AppRoutingModule,
             HttpClientModule,
             NgxChartsModule],
